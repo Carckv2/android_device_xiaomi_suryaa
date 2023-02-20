e29301cca1a14fefb91bb8e18fe2563e77ff1019
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-02-20 21:06:07 +0000

    VOLTAGE WELCOME

--- voltage_surya.mk
+++ voltage_surya.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common BananaDroid stuff.
-$(call inherit-product, vendor/banana/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := banana_surya
+PRODUCT_NAME := voltage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
@@ -23,12 +23,14 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # BananaDroid
-BANANA_BUILD_TYPE := UNOfficial
-BANANA_MAINTAINER := NINJA
+VOLTAGE_BUILD_TYPE := UNOfficial
+VOLTAGE_MAINTAINER := NINJA
 TARGET_ENABLE_BLUR := true
-#WITH_GAPPS := true
-#TARGET_GAPPS_ARCH := arm64
-#TARGET_CORE_GAPPS := true
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_CORE_GAPPS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 USE_PIXEL_CHARGING := true
 TARGET_INCLUDE_CARRIER_SETTINGS := true
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
